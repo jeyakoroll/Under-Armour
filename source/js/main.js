18303e4b4267d20435a5da48b60d52e2996bfae5
Korolenko Evgeniy <jeyakoroll@gmail.com>
2017-11-29 17:22:46 +0300

    gallery is done

---
+++ source/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+
+	// style for form
+    $( '.gallery__man-form .order-form' ).addClass( 'footer__form-order' );
+    $( '.gallery__man-form input' ).addClass( 'footer__form-data' );
+    $( '.gallery__man-form button' ).addClass( 'footer__form-button' );
+    $('.gallery__man-form input[name=name_first]').attr("placeholder","Имя");
+    $('.gallery__man-form input[name=name_last]').attr("placeholder","Телефон");
+
+
+}); 	
